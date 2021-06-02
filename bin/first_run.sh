54c1b31e341b24a2cc238b22527da5f60f65105d
Yannick Marcon <yannick.marcon@obiba.org>
2021-06-02 09:21:07 +0200

    log formatting

--- bin/first_run.sh
+++ bin/first_run.sh
@@ -8,7 +8,7 @@ if [ -n "$ROCK_SERVER" ]
 then
 	sed s,@rock_server@,$ROCK_SERVER,g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 else
-	echo "[Warn] No server public address: self-registration not activated"
+	echo "[Warn]   No server public address: self-registration not activated"
 	sed s,@rock_server@,,g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 fi
 mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
@@ -17,7 +17,7 @@ if [ -n "$ROCK_ID" ]
 then
 	sed s/@rock_id@/$ROCK_ID/g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 else
-	echo "[Warn] Setting default node id: rserver"
+	echo "[Warn]   Setting default node id: rserver"
 	sed s/@rock_id@/rserver/g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 fi
 mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
@@ -26,7 +26,7 @@ if [ -n "$ROCK_CLUSTER" ]
 then
 	sed s/@rock_cluster@/$ROCK_CLUSTER/g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 else
-	echo "[Info] Setting default node cluster: default"
+	echo "[Info]   Setting default node cluster: default"
 	sed s/@rock_cluster@/default/g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 fi
 mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
@@ -35,7 +35,7 @@ if [ -n "$ROCK_TAGS" ]
 then
 	sed s/@rock_tags@/$ROCK_TAGS/g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 else
-	echo "[Info] Setting empty node tags"
+	echo "[Info]   Setting empty node tags"
 	sed s/@rock_tags@//g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 fi
 mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
@@ -46,7 +46,7 @@ if [ -n "$ROCK_CONSUL_SERVER" ]
 then
 	sed s,@rock_consul_server@,$ROCK_CONSUL_SERVER,g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 else
-	echo "[Info] No Consul server: self-registration not activated"
+	echo "[Info]   No Consul server: self-registration not activated"
 	sed s,@rock_consul_server@,,g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 fi
 mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
@@ -55,7 +55,7 @@ if [ -n "$ROCK_CONSUL_TOKEN" ]
 then
 	sed s,@rock_consul_token@,$ROCK_CONSUL_TOKEN,g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 else
-	echo "[Warn] No Consul token: self-registration not safe"
+	echo "[Warn]   No Consul token: self-registration not safe"
 	sed s,@rock_consul_token@,,g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 fi
 mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
@@ -66,7 +66,7 @@ if [ -n "$ROCK_OPAL_SERVER" ]
 then
 	sed s,@rock_opal_server@,$ROCK_OPAL_SERVER,g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 else
-	echo "[Info] No Opal server: self-registration not activated"
+	echo "[Info]   No Opal server: self-registration not activated"
 	sed s,@rock_opal_server@,,g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 fi
 mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
@@ -75,7 +75,7 @@ if [ -n "$ROCK_OPAL_TOKEN" ]
 then
 	sed s,@rock_opal_token@,$ROCK_OPAL_TOKEN,g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 else
-	echo "[Warn] No OPAL token: self-registration not valid"
+	echo "[Warn]   No Opal token: self-registration not valid"
 	sed s,@rock_opal_token@,,g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 fi
 mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
@@ -86,7 +86,7 @@ if [ -n "$ROCK_REPOS" ]
 then
 	sed 's;@rock_repos@;$ROCK_REPOS;g' $ROCK_HOME/conf/application.yml > /tmp/application.yml
 else
-	echo "[Info] No R repositories, setting default"
+	echo "[Info]   No R repositories, setting default"
 	sed 's;@rock_repos@;https://cloud.r-project.org,https://cran.obiba.org;g' $ROCK_HOME/conf/application.yml > /tmp/application.yml
 fi
 mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
@@ -102,12 +102,12 @@ then
 	then
 		sed s/@rock_administrator_password@/$ROCK_ADMINISTRATOR_PASSWORD/g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 	else
-		echo "[Info] Setting default administrator password: password"
+		echo "[Info]   Setting default administrator password: password"
 		sed s/@rock_administrator_password@/password/g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 	fi
 	mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
 else
-	echo "[Info] No administrator configured!"
+	echo "[Info]   No administrator configured!"
 	sed s/@rock_administrator_name@//g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 	mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
 	sed s/@rock_administrator_password@//g $ROCK_HOME/conf/application.yml > /tmp/application.yml
@@ -122,12 +122,12 @@ then
 	then
 		sed s/@rock_manager_password@/$ROCK_MANAGER_PASSWORD/g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 	else
-		echo "[Info] Setting default manager password: password"
+		echo "[Info]   Setting default manager password: password"
 		sed s/@rock_manager_password@/password/g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 	fi
 	mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
 else
-	echo "[Info] No manager configured!"
+	echo "[Info]   No manager configured!"
 	sed s/@rock_manager_name@//g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 	mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
 	sed s/@rock_manager_password@//g $ROCK_HOME/conf/application.yml > /tmp/application.yml
@@ -142,12 +142,12 @@ then
 	then
 		sed s/@rock_user_password@/$ROCK_USER_PASSWORD/g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 	else
-		echo "[Info] Setting default user password: password"
+		echo "[Info]   Setting default user password: password"
 		sed s/@rock_user_password@/password/g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 	fi
 	mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
 else
-	echo "[Info] No user configured!"
+	echo "[Info]   No user configured!"
 	sed s/@rock_user_name@//g $ROCK_HOME/conf/application.yml > /tmp/application.yml
 	mv -f /tmp/application.yml $ROCK_HOME/conf/application.yml
 	sed s/@rock_user_password@//g $ROCK_HOME/conf/application.yml > /tmp/application.yml
